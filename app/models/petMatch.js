d5512231d5381d3657823e4f1497174d12c07bfb
Robin Bertuccelli <robin.bertuccelli@gmail.com>
2018-03-15 10:37:07 -0500

    aded handlebars to file structure

---
+++ app/models/petMatch.js
@@ -0,0 +1,8 @@
+// Dependencies
+// =============================================================
+
+// This may be confusing but here Sequelize (capital) references the standard library
+var Sequelize = require("sequelize");
+// sequelize (lowercase) references our connection to the DB.
+var sequelize = require("../config/connection.js");
+
