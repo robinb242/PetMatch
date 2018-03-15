05b2e402097bcfc9c07cdbb4dbecaaf97dad415a
Robin Bertuccelli <robin.bertuccelli@gmail.com>
2018-03-15 15:56:59 -0500

    petMatchSequelize

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,16 +1,17 @@
 INSERT INTO animal_traits (animal, trait_kids, trait_active, trait_security, trait_independent, trait_cuddly, trait_large, trait_medium, trait_small) 
-VALUES ('cat', true, false, false, true, true, false, true, true);
-VALUES ('rabbit', true, false, false, true, true, false, true, true);
-VALUES	('hamster', true, false, false, true, true, false, false, true);
-VALUES	('rat', true, false, false, true, true, false, false, true);
-VALUES	('mouse', true, false, false, true, true, false, false, true);
-VALUES	('snake', false, false, false, false, false, false, true, false);
-VALUES	('fish', false, false, false, false, false, false, false, true);
-VALUES ('bernese mountain dog', true, false, true, false, true, true, false, false);
-VALUES ('retriever', true, true, true, false, true, true, true, false);
-VALUES ('doberman', false, true, true, false, false, true, false, false);
-VALUES ('german shephard', false, true, false, false, true, true, false, false);
-VALUES ('rottweiler', false, true, true, false, false, true, false, false);
-VALUES ('boxer', false, true, true, false, false, true, false, false);
+
+VALUES ('cat', true, false, false, true, true, false, true, true),
+VALUES ('rabbit', true, false, false, true, true, false, true, true),
+VALUES	('hamster', true, false, false, true, true, false, false, true),
+VALUES	('rat', true, false, false, true, true, false, false, true),
+VALUES	('mouse', true, false, false, true, true, false, false, true),
+VALUES	('snake', false, false, false, false, false, false, true, false),
+VALUES	('fish', false, false, false, false, false, false, false, true),
+VALUES ('bernese mountain dog', true, false, true, false, true, true, false, false),
+VALUES ('retriever', true, true, true, false, true, true, true, false),
+VALUES ('doberman', false, true, true, false, false, true, false, false),
+VALUES ('german shephard', false, true, false, false, true, true, false, false),
+VALUES ('rottweiler', false, true, true, false, false, true, false, false),
+VALUES ('boxer', false, true, true, false, false, true, false, false),
 VALUES ('mastiff', true, false, true, false, true, true, false, false);
 
