6b8c4f922228a7159680af8541e12338d626dac5
Phil S <philipstubbs13@gmail.com>
2018-03-16 22:07:10 -0500

    got server running again

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+//Requiring models
+var db = require("./models");
+
+//Define port to listen on.
 var PORT = process.env.PORT || 3000;
 
+//App is using express.
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -21,10 +26,11 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/appController.js");
+require("./controllers/appController.js")(app);
 
-app.use(routes);
-
-app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
+//App is listening...
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App now listening at localhost:" + PORT);
+  });
 });
