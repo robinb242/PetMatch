c31bb63720a4c9f2215746e21c66c3f3cf315df3
Phil S <philipstubbs13@gmail.com>
2018-03-23 08:43:07 -0500

    added second controller file for saved pets feature

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-require("./controllers/appController.js")(app);
+require("./controllers/petMatchController.js")(app);
+require("./controllers/savedPetsController.js")(app);
 
 
 //App is listening...
