c71858380677d765e6154250331fd486c31d43da
Phil S <philipstubbs13@gmail.com>
2018-03-27 09:11:44 -0500

    heroku

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ require("./controllers/savedPetsController.js")(app);
 
 
 //App is listening...
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force: true}).then(function() {
   app.listen(PORT, function() {
     console.log("App now listening at localhost:" + PORT);
   });
